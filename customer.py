7b68f70be1baf8ba77e04ad3c75bb7d9abd7f30f
REZZ <turbo.emamhasani@gmail.com>
2023-12-31 00:07:38 +0330

    add details to customer

--- customer.py
+++ customer.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk #
-
+from customtkinter import CTkOptionMenu
 class cust_win:
     def __init__(self,root):
         self.root=root
@@ -42,10 +42,30 @@ class cust_win:
         entry_ref=Entry(labelframeleft,width=22,font=("Vazir",12,))
         entry_ref.grid(row=2,column=1)
 
+        # jensiat
+        # made by custom tkinter - gender choose with optionmenu_callback
+        lbl_gender=Label(labelframeleft,text="جنسیت",font=("Vazir",12,),padx=2,pady=6)
+        lbl_gender.grid(row=3,column=0,sticky="w")
+
+        gender_menu = CTkOptionMenu(labelframeleft, values=["آقا", "خانم"],width=205,font=("Vazir",16),corner_radius=0,dropdown_font=("Vazir",18),
+                                         fg_color='#ffffff',text_color='black',button_color='grey',
+                                         command=self.optionmenu_callback)
+        gender_menu.set("آقا")
+        gender_menu.grid(row=3,column=1)
+
+
+        # phone number 1
+        lbl_phone_num=Label(labelframeleft,text="شماره تماس",font=("Vazir",12,),padx=2,pady=6)
+        lbl_phone_num.grid(row=4,column=0,sticky="w")
+
+        entry_ref=Entry(labelframeleft,width=22,font=("Vazir",12,))
+        entry_ref.grid(row=4,column=1)
 
 
 
-        
+
+    def optionmenu_callback(self,choice):
+        print("optionmenu dropdown clicked:", choice)     
 
     def close_window(self, event):
         self.root.destroy()
